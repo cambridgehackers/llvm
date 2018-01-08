98cf4c61f5499859a8eec724cb40201c6879b6b2
John Ankcorn <jca@alum.mit.edu>
2018-01-08 10:39:14 -0800

    resolve generated __rule name conflicts

--- lib/Target/Atomicc/AtomiccRuntime.cpp
+++ lib/Target/Atomicc/AtomiccRuntime.cpp
@@ -328,13 +328,20 @@ extern "C" void *llvm_translate_malloc(size_t size, Type *type, const StructType
  */
 extern "C" void addBaseRule(const char *name, uint64_t *bcap, Function *ardyFunc, Function *aenaFunc)
 {
+    std::string tempName = name;
+    for (int i = 0; i < aenaFunc->arg_size() - 1; i++)
+        tempName += "_" + utostr(bcap[i]);
     Function *rdyFunc = fixupFunction(bcap, ardyFunc);
     Function *enaFunc = fixupFunction(bcap, aenaFunc);
-    std::string enaName = name;
     ClassMethodTable *table = classCreate[findThisArgument(rdyFunc)];
-    table->ruleFunctions[name] = enaFunc;
+    std::string enaName = tempName;
+    int counter = 100;
+    // if necessary to avoid conflicts, generate unique rule names
+    while (table->method[enaName + "__ENA"])
+        enaName = tempName + "$" + utostr(counter++);
+    table->ruleFunctions[enaName] = enaFunc;
     if (trace_pair)
-        printf("[%s:%d] name %s ena %s rdy %s\n", __FUNCTION__, __LINE__, name, enaFunc->getName().str().c_str(), rdyFunc->getName().str().c_str());
+        printf("[%s:%d] name %s size %d ena %s rdy %s\n", __FUNCTION__, __LINE__, enaName.c_str(), aenaFunc->arg_size(), enaFunc->getName().str().c_str(), rdyFunc->getName().str().c_str());
     pushPair(enaFunc, enaName + "__ENA", rdyFunc, enaName + "__RDY");
 }
 
